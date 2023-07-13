c7a22ffcc414f49ce8afa7eac279ab8886467e12
Dennis Zhitenev <denniszhitenev@gmail.com>
2023-07-13 01:11:02 -0400

    Implemented the Google API request function
    Added the ability to actually use the Google Cloud API. Did this using Google's Python module instead of an HTTP request. Since their module doesn't return JSON like the HTTP request, some extra processing had to be added to keep compatibility with manually pasting in JSON. Switching between modes was made more robust using enums. Lastly, the text processing now removes dashes (they mess with the TTS) and replaces decimal points with the word "point".

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import re
 import time
-import requests
 import json
 import base64
 import subprocess
@@ -8,45 +7,39 @@ import random
 import praw
 from tiktok_uploader.upload import upload_videos
 from tiktok_uploader.auth import AuthBackend
+from google.cloud import texttospeech_v1beta1 as tts
+from enum import Enum
+
+class Mode(Enum):
+    API = 0
+    MANUAL = 1
+    TESTING = 2
 
 def post_to_ssml(split_post):
     ssml_text = ""
     i = 0
 
     for phrase in split_post:
-        ssml_text += "<mark name='" + str(i) + "'/>" + phrase.replace("/", " slash ")
+        processed_phrase = phrase.replace("/", " slash ").replace("-", " ")
+        processed_phrase = re.sub("(?<=[0-9])\.(?=[0-9])", " point ", processed_phrase) # Replace decimal points in numbers with the word "point"
+        ssml_text += "<mark name='" + str(i) + "'/>" + processed_phrase
         i += 1
 
     return "<speak>" + ssml_text + "<mark name='" + str(i) + "'/>" + "</speak>"
 
 def google_api_request(ssml_text):
+    client = tts.TextToSpeechClient()
+    voice = tts.VoiceSelectionParams(language_code="en-US", name="en-US-Standard-J")
+    audio_config = tts.AudioConfig(audio_encoding=tts.AudioEncoding.MP3_64_KBPS, pitch=-5.0, speaking_rate=1.3)
 
-    url = "https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=[YOUR_API_KEY]"
-    headers = {
-        "Authorization": "Bearer [YOUR_ACCESS_TOKEN]",
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-
-    data = {
-        "audioConfig": {
-            "audioEncoding": "MP3_64_KBPS",
-            "pitch": -5.0,
-            "speakingRate": 1.3
-        },
-        "input": {
-            "ssml": ssml_text
-        },
-        "voice": {
-            "languageCode": "en-US",
-            "name": "en-US-Standard-J"
-        },
-        "enableTimePointing": [
-            "SSML_MARK"
-        ]
-    }
-
-    return requests.post(url, headers=headers, data=data)
+    return client.synthesize_speech(
+        request=tts.SynthesizeSpeechRequest(
+            input=tts.SynthesisInput(ssml=ssml_text),
+            voice=voice,
+            audio_config=audio_config,
+            enable_time_pointing = [tts.SynthesizeSpeechRequest.TimepointType.SSML_MARK]
+        )
+    )
 
 def manual_request(ssml_text):
 
@@ -72,14 +65,10 @@ def manual_request(ssml_text):
         json.dump(data, f)
 
     input("Paste the response into JSON_copy_paste.txt and hit enter")
-
-    with open("intermediates/JSON_copy_paste.txt", "r") as f:
-        return json.load(f)
     
-def decode_audio(base64_audio):
-    audio = base64.b64decode(base64_audio)
+def save_audio(binary_audio):    
     with open("intermediates/voice.mp3", "wb") as out:
-        out.write(audio)
+        out.write(binary_audio)
 
 def break_long_phrases(split_post):
     i = 0
@@ -126,52 +115,60 @@ def get_file_length(filename):
 def print_error(string):
     print('\033[91m' + string + '\033[0m')
 
-reddit = praw.Reddit("bot1")
+# Set program mode
+current_mode = Mode.TESTING
 
+# Obtain text from reddit post
+reddit = praw.Reddit("bot1")
 # for submission in reddit.subreddit("AmITheAsshole").top(time_filter="month"):
-for submission in reddit.subreddit("AmITheAsshole").hot(limit=10):
+for submission in reddit.subreddit("AmITheAsshole").hot():
     if not submission.stickied and "http" not in submission.selftext:
         post_text = submission.title + " " + submission.selftext
         break
 
-post_text += "." # Ensure text ends with punctuation so that the last phrase is captured
+post_text = post_text.strip() + "." # Ensure text ends with punctuation so that the last phrase is captured
 post_text = post_text.replace("’", "'").replace("‘", "'").replace("“", '"').replace("”", '"') # Swap out unsupported quotation marks
 post_text = re.sub("\s*\n", ". ", post_text) # Replace paragraph breaks with periods
-split_post = re.findall("[^.]+?[.,?!-]", post_text) # Split post into phrases followed by a punctuation mark
+split_post = re.findall("[^.]+?[.,?!][0-9]*", post_text) # Split post into phrases followed by a punctuation mark
 break_long_phrases(split_post)
 
 ssml_text = post_to_ssml(split_post)
 
-#######################################################
+if current_mode == Mode.API:
 
-# API request
-# response = google_api_request(ssml_text)
+    response = google_api_request(ssml_text)
+    save_audio(response.audio_content)
+    times = []
+    for timepoint in response.timepoints:
+        times.append({"markName": timepoint.mark_name, "timeSeconds": timepoint.time_seconds})
 
-# Manual request
-# response = manual_request(ssml_text)
+    with open("intermediates/JSON_copy_paste.txt", "w") as f:
+        json.dump({"audioContent": base64.b64encode(response.audio_content).decode(), "timepoints": times}, f)
+    
+else:
+    
+    if current_mode == Mode.MANUAL:
+        response = manual_request(ssml_text)
 
-# Testing
-with open("intermediates/JSON_copy_paste.txt", "r") as f:
-    response = json.load(f)
-
-#######################################################
-
-if "error" in response:
-    print_error("****Google Cloud TTS API error****")
-    quit()
+    with open("intermediates/JSON_copy_paste.txt", "r") as f:
+        response = json.load(f)
+    
+    save_audio(base64.b64decode(response["audioContent"]))
+    times = response["timepoints"]
 
 bg_video_name = random.choice(["minecraft.mp4", "subway.mp4"])
 video_length = get_file_length("background_videos/" + bg_video_name)
 audio_length = get_file_length("intermediates/voice.mp3")
 
-decode_audio(response["audioContent"])
 subprocess.run("ffmpeg -y -ss " + sec_to_hms(random.randrange(0, int(video_length - audio_length - 1)))
                + " -i background_videos/" + bg_video_name + " -i intermediates/voice.mp3 -c copy -map 0:v:0 -map 1:a:0 -shortest intermediates/video_no_text.mp4")
 
-times = response["timepoints"]
 create_subtitles(split_post, times)
 
 subprocess.run("ffmpeg -y -i intermediates/video_no_text.mp4 -vf \"subtitles=intermediates/subtitles.srt:force_style='Fontname=Montserrat Black,Alignment=10,Shadow=1,MarginL=90,MarginR=90:charenc=ISO8859-1'\" -c:a copy result/final.mov")
 
 upload_videos([{"path": "result/final.mov", "description": "#reddit #reddittiktok #redditreading #redditposts #redditstories #fyp #xyzbca "}],
-              auth=AuthBackend(cookies="cookies.txt"), headless=False)
+              auth=AuthBackend(cookies="private/cookies.txt"), headless=False)
+
+# TODO: add SQL database for storing video ids that have already been posted
+# TODO: set up raspberry pi to run this code every few hours as a test
